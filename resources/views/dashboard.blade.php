2b9d7956fe19fea28b6f0ec6018b21f45bddaeba
ilimiBack <dev.backend@ilimigroup.com>
2025-10-08 17:46:58 +0000

    Mise a jour

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -23,159 +23,197 @@
        <!-- Main Content -->
 <main class="flex-1 p-10 bg-[#F3F3F3] rounded-tl-1xl">
 
-        <!-- La premeiere drande carte -->
-    <div class="flex p-8 justify-around rounded-lg border border-1 border-[#989898] ">
+        <!-- La premeiere grande carte -->
+    <div class=" p-8 justify-around rounded-lg border border-1 border-[#989898] ">
+        <div class="grid grid-cols-1 sm:grid-cols-2 mt-2 gap-x-80">
 
-         <div class="flex items-center bg-[#1FB19E] p-4 w-[400px] rounded-lg space-x-2 justify-between">
-            <div class="flex items-center space-x-2"> 
+            <div class="flex flex-wrap items-center bg-[#1FB19E] p-4  w-auto  rounded-lg space-x-2 justify-between mb-7 left-32">
+                <div class="flex items-center space-x-2"> 
                 <img class=" h-7" src="images/users.png" alt="">
-                <h3 class="text-white">Total Employés</h3>
+                <h3 class="text-white text-sm sm:text-base md:text-lg">Total Employés</h3>
+                </div>
+             <p class="ml-auto font-bold text-white text-xl sm:text-2xl ">{{ $totalEmployes ?? '' }}</p>
             </div>
-             <p class="ml-auto text-3xl font-bold text-white">{{ $totalEmployes ?? '' }}</p>
-        </div>
         
 
-        <div class="flex bg-[#272C62] p-4 w-[400px] rounded-lg space-x-2 justify-between">
+        <div class="flex flex-wrap bg-[#272C62] p-4 w-auto rounded-lg space-x-2 justify-between mb-7">
 
             <div class="flex items-center space-x-2">
                  <img class="h-7" src="images/users.png" alt=""> 
-                <h3 class="text-white">Total Licences</h3>
+                <h3 class="text-white text-sm sm:text-base md:text-lg">Total Licences</h3>
             </div>
 
-             <p class="text-3xl font-bold text-white">{{ $totalLicences ?? '' }}</p>
+             <p class="text-xl sm:text-2xl font-bold text-white">{{ $totalLicences ?? '' }}</p>
+        </div>
+
         </div>
 
     </div> <br>
 
-    <!-- La deuxieme grande carte -->
-<div class="rounded-lg border border-1 border-[#989898] p-10">
+        <!-- La deuxieme grande carte -->
+    <div class="rounded-lg border border-1 border-[#989898] w-full p-10">
+    <div class="grid grid-cols-1 sm:grid-cols-2 mt-2 gap-x-80 gap-y-2">
 
-            <!-- La div1 -->
-    <div class="flex justify-evenly  ">
-                <!-- La carte1 -->
-        <div class="flex items-center bg-white p-4 w-[400px] rounded-lg space-x-2 border-[#989898] justify-between">
+             <!-- La carte1 -->
+        <div class="flex flex-wrap items-center bg-white p-4 w-full rounded-lg space-x-2 justify-between mb-7">           
             <div class="flex items-center space-x-2 ">
                 <img class=" h-6" src="images/Group 1.png" alt="">
-                <h3 class="text-[#585858] ">Total Équipements</h3>
+                <h3 class="text-[#585858] text-sm sm:text-base md:text-lg">Total Équipements</h3>
             </div>
-                 <p class="ml-auto text-2xl text-[#585858] left-80">. {{ $totalEquipements ?? '' }}</p>
+                <div class=" flex space-x-2">
+                    <div class=" rounded-full bg-[#989898] p-1 w-1 h-1 mt-3">
+                    </div>
+                 <p class="text-xl sm:text-2xl text-[#1FB19E] font-semibold"> {{ $totalEquipements ?? '' }}</p>     
+
+                </div>
+                
         </div>
 
           <!-- La carte2 -->
-        <div class="flex bg-white p-4 w-[400px] rounded-lg space-x-2 justify-between">
+        <div class="flex flex-wrap bg-white p-4 w-auto rounded-lg space-x-2 justify-between mb-7">
             <div class="flex items-center space-x-2">
                 <img class="h-7" src="images/Group 1.png" alt=""> 
-                <h3 class="text-[#585858] ">Équipements Actifs</h3>
+                <h3 class="text-[#585858] text-sm sm:text-base md:text-lg">Équipements Actifs</h3>
             </div>
-                <p class="text-2xl  text-[#585858] ">.{{  $equipementsActifs ?? '' }}</p>
+                <div class=" flex space-x-2">
+                    <div class=" rounded-full bg-[#989898] p-1 w-1 h-1 mt-3">
+                    </div>
+                    <p class="text-xl sm:text-2xl text-[#1FB19E] ">{{  $equipementsActifs ?? '' }}</p>
 
+                </div>
         </div>
 
-    </div> <br>
-
-                    <!-- La div2 -->
-      <div class="flex justify-evenly">
-        <div class="flex items-center bg-white p-4 w-[400px] rounded-lg space-x-2 border-[#989898] justify-between">
+        <!-- La carte3 -->
+        <div class="flex flex-wrap items-center bg-white p-4  w-auto  rounded-lg space-x-2 border-[#989898] justify-between mb-7">
             <div class="flex items-center space-x-2"> 
                 <img class=" h-6" src="images/Group 1.png" alt="">
-                <h3 class="text-[#585858] ">Équipements en Panne</h3>
+                <h3 class="text-[#585858] text-sm sm:text-base md:text-lg">Équipements en Panne</h3>
             </div> 
 
-             <p class="ml-auto text-2xl text-[#585858] ">.{{ $equipementsEnPanne ?? '' }}</p>
+                <div class=" flex space-x-2">
+                    <div class=" rounded-full bg-[#989898] p-1 w-1 h-1 mt-3">
+                    </div>
+                    <p class="text-xl sm:text-2xl text-[#1FB19E] ">{{ $equipementsEnPanne ?? '' }}</p>
+
+                </div>
+
         </div>
 
-
-        <div class="flex bg-white p-4 w-[400px] rounded-lg space-x-2  justify-between">
+            <!-- La carte4 -->
+        <div class="flex flex-wrap bg-white p-4  w-auto  rounded-lg space-x-2  justify-between mb-7">
 
             <div class="flex items-center space-x-2">
                  <img class="h-7" src="images/Group 1.png" alt=""> 
-                <h3 class="text-[#585858] ">Équipements Hors D'usage</h3>
+                <h3 class="text-[#585858] text-sm sm:text-base md:text-lg">Équipements Hors D'usage</h3>
             </div>
+                <div class=" flex space-x-2">
+                    <div class=" rounded-full bg-[#989898] p-1 w-1 h-1 mt-3">
+                    </div>
+                    <p class="text-xl sm:text-2xl text-[#1FB19E] ">{{  $equipementsHorsService ?? '' }}</p>
 
-             <p class="text-2xl text-[#585858] ">.{{  $equipementsHorsService ?? '' }}</p>
+                </div>
+
         </div>
 
-      </div> <br>
-
-
-                    <!-- La div3 -->
-      <div class="flex justify-evenly">
-        <div class="flex items-center bg-white p-4 w-[400px] rounded-lg space-x-2 border-[#989898] justify-between">
+            <!-- La carte5 -->
+        <div class="flex flex-wrap items-center bg-white p-4  w-auto  rounded-lg space-x-2 border-[#989898] justify-between mb-7">
             <div class="flex items-center space-x-2"> 
                 <img class=" h-6" src="images/Group 1.png" alt="">
-                <h3 class="text-[#585858] ">Total Maintenances</h3>
+                <h3 class="text-[#585858] text-sm sm:text-base md:text-lg ">Total Maintenances</h3>
             </div> 
+                <div class=" flex space-x-2">
+                    <div class=" rounded-full bg-[#989898] p-1 w-1 h-1 mt-3">
+                    </div>
+                     <p class="text-xl sm:text-2xl text-[#1FB19E] ">{{ $totalMaintenances ?? '' }}</p>
 
-             <p class="ml-auto text-2xl text-[#585858] ">.{{ $totalMaintenances ?? '' }}</p>
+                </div>
+
         </div>
 
-
-        <div class="flex bg-white p-4 w-[400px] rounded-lg space-x-2  justify-between">
+            <!-- La carte6 -->
+        <div class="flex flex-wrap bg-white p-4  w-auto rounded-lg space-x-2  justify-between mb-7">
 
             <div class="flex items-center space-x-2">
                  <img class="h-7" src="images/Group 1.png" alt=""> 
-                <h3 class="text-[#585858] ">Maintenances en Cours</h3>
+                <h3 class="text-[#585858] text-sm sm:text-base md:text-lg">Maintenances en Cours</h3>
             </div>
+                <div class=" flex space-x-2">
+                    <div class=" rounded-full bg-[#989898] p-1 w-1 h-1 mt-3">
+                    </div>
+                    <p class="text-xl sm:text-2xl text-[#1FB19E] ">{{  $maintenancesEnCours ?? '' }}</p>
 
-             <p class="text-2xl text-[#585858] ">.{{  $maintenancesEnCours ?? '' }}</p>
+                </div>
+
         </div>
 
-      </div> <br>
-
-
-                    <!-- La div4 -->
-      <div class="flex justify-evenly">
-        <div class="flex items-center bg-white p-4 w-[400px] rounded-lg space-x-2 border-[#989898] justify-between">
+             <!-- La carte7 -->
+        <div class="flex flex-wrap items-center bg-white p-4  w-auto  rounded-lg space-x-2 border-[#989898] justify-between mb-7">
             <div class="flex items-center space-x-2"> 
                 <img class=" h-6" src="images/Group 1.png" alt="">
-                <h3 class="text-[#585858] ">Maintenances Terminées</h3>
+                <h3 class="text-[#585858] text-sm sm:text-base md:text-lg">Maintenances Terminées</h3>
             </div> 
+                <div class=" flex space-x-2">
+                    <div class=" rounded-full bg-[#989898] p-1 w-1 h-1 mt-3">
+                    </div>
+                    <p class="text-xl sm:text-2xl text-[#1FB19E] ">{{ $maintenancesTerminees ?? '' }}</p>
 
-             <p class="ml-auto text-2xl text-[#585858] ">.{{ $maintenancesTerminees ?? '' }}</p>
+                </div>
+
         </div>
-
-
-        <div class="flex bg-white p-4 w-[400px] rounded-lg space-x-2 justify-between ">
+            
+            <!-- La carte8 -->
+        <div class="flex flex-wrap bg-white p-4  w-auto rounded-lg space-x-2 justify-between mb-7">
 
             <div class="flex items-center space-x-2">
                  <img class="h-7" src="images/Group 1.png" alt=""> 
-                <h3 class="text-[#585858] ">Maintenances En Attente</h3>
+                <h3 class="text-[#585858] text-sm sm:text-base md:text-lg">Maintenances En Attente</h3>
             </div>
+                <div class=" flex space-x-2">
+                    <div class=" rounded-full bg-[#989898] p-1 w-1 h-1 mt-3">
+                    </div>
+                    <p class="text-xl sm:text-2xl text-[#1FB19E] ">{{  $maintenancesEnAttente ?? '' }}</p>
 
-             <p class="text-2xl text-[#585858] ">.{{  $maintenancesEnAttente ?? '' }}</p>
+                </div>
+
         </div>
 
-      </div> <br>
-
-
-                    <!-- La div5 -->
-      <div class="flex justify-evenly">
-        <div class="flex items-center bg-white p-4 w-[400px] rounded-lg space-x-2 border-[#989898] justify-between">
+        <!-- La carte9 -->
+        <div class="flex flex-wrap items-center bg-white p-4  w-auto  rounded-lg space-x-2 border-[#989898] justify-between mb-7">
             <div class="flex items-center space-x-2"> 
                 <img class=" h-6" src="images/Group 1.png" alt="">
-                <h3 class="text-[#585858] ">Maintenances Annulées</h3>
+                <h3 class="text-[#585858] text-sm sm:text-base md:text-lg">Maintenances Annulées</h3>
             </div> 
+                <div class=" flex space-x-2">
+                    <div class=" rounded-full bg-[#989898] p-1 w-1 h-1 mt-3">
+                    </div>
+                    <p class="text-xl sm:text-2xl text-[#1FB19E] ">{{ $maintenancesAnnulees ?? '' }}</p>
 
-             <p class="ml-auto text-2xl text-[#585858] ">.{{ $maintenancesAnnulees ?? '' }}</p>
+                </div>
+
         </div>
 
-
-        <div class="flex bg-white p-4 w-[400px] rounded-lg space-x-2 justify-between">
+            <!-- La carte10 -->
+        <div class="flex flex-wrap items-center bg-white p-4  w-auto  rounded-lg space-x-2 border-[#989898] justify-between mb-7">
 
             <div class="flex items-center space-x-2">
                  <img class="h-7" src="images/Group 1.png" alt=""> 
-                <h3 class="text-[#585858] ">Maintenances Reportées</h3>
+                <h3 class="text-[#585858] text-sm sm:text-base md:text-lg">Maintenances Reportées</h3>
             </div>
+                <div class=" flex space-x-2">
+                    <div class=" rounded-full bg-[#989898] p-1 w-1 h-1 mt-3">
+                    </div>
+                    <p class="text-xl sm:text-2xl text-[#1FB19E] ">{{  $maintenancesReporte ?? '' }}</p>
 
-             <p class="text-2xl text-[#585858] ">.{{  $maintenancesReporte ?? '' }}</p>
+                </div>
+
         </div>
+            
+        </div>
+    </div> <br> <br> <br>
 
-      </div> 
-
-</div> <br> <br>
         <div class="flex justify-center items-center">
             <img src="{{ asset('images/logoilimiGroup.png') }}" alt="Logo" class="h-6">
-        </div>
+</div>
 
 </main>
 
